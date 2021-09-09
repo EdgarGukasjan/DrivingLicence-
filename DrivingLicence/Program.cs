5e7d56391f392164cd99978b3c3b888321797157
Opilane TTHK <opilane@tthk.ee>
2021-09-09 10:35:02 +0300

    Add Control Flow

---
+++ DrivingLicence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DrivingLicence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //program asks date of birth 
+            //program controlls users age
+            //program teatab kasutajat, kas ta on piisavalt vana,
+            //to have drivers licence (if 18 years old)
+
+            Console.WriteLine("Whats your date of birth?");
+            
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+            
+            int Age = 2021 - YearOfBirth;
+
+            Console.WriteLine("You were born in " + YearOfBirth);
+
+            if(Age > 18)
+            {
+                Console.WriteLine("Old enough for drivers licence, congrats");
+            }
+            else if(Age < 18)
+            {
+                Console.WriteLine("Too young bitch, grow up");
+            }
+            else
+            {
+                Console.WriteLine("Old enough for drivers licence, congrats");
+            }
+        }
+    }
+}
---
+++ WhenWereYouBorn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WhenWereYouBorn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //program küsib kasutaja vanust 
+            //kui kasutaja on vanem, kui 18, siis konsolle kuvatakse "oled piisavalt vane"
+            //kui kasutaja on noorem, kui 18, siis konsoolis kuvatakse "oled liiga noor"
+            //kui kasutaja on 18, siis konsoolis kuvatakse "oledki 18"
+
+            Console.WriteLine("Enter your age");
+
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            if (Age > 18)
+            {
+                Console.WriteLine("Old enough");
+            }
+            else if (Age < 18)
+            {
+                Console.WriteLine("You are too young");
+            }
+            else
+            {
+                Console.WriteLine("You are 18");
+            }
+
+            Console.WriteLine("Have a nice day bitch");
+
+
+        }
+    }
+}
